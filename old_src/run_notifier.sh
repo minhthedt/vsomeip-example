c2761e0a5608083ae8e946cc21d4a2e05f8971ca
minhthedt <minhthedt@gmail.com>
2025-06-20 09:32:11 +0900

    old source

---
+++ old_src/run_notifier.sh
@@ -0,0 +1,4 @@
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export VSOMEIP_APPLICATION_NAME=notify-sample
+export VSOMEIP_CONFIGURATION=/home/worker/TRAINING/VSOMEIP-example/vsomeip_notifier.json
+ ./notify-sample
