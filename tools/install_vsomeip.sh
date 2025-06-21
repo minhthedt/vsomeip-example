fa363d5dab437c871b00a03d28d91f97d10ef4da
minhthedt <minhthedt@gmail.com>
2025-06-21 09:35:43 +0900

    add gitignore

--- tools/install_vsomeip.sh
+++ tools/install_vsomeip.sh
@@ -5,9 +5,9 @@ required_version="3.20.0"
 
 if [[ -z "$cmake_version" || "$(printf '%s\n' "$required_version" "$cmake_version" | sort -V | head -n 1)" != "$required_version" ]]; then
     echo "CMake version is less than $required_version or not installed. Installing CMake 3.20.2..."
-    rm -rf build_cmake
-    mkdir build_cmake
-    pushd build_cmake
+    rm -rf cmake-3.20.2
+    mkdir cmake-3.20.2
+    pushd cmake-3.20.2
     sudo apt install build-essential libssl-dev -y
     wget https://github.com/Kitware/CMake/releases/download/v3.20.2/cmake-3.20.2.tar.gz
     tar -zxvf cmake-3.20.2.tar.gz
@@ -26,22 +26,34 @@ else
     echo "CMake version $cmake_version is sufficient."
 fi
 
-# i want to istall boost, vsomeip to custom directory
-INSTALL_DIR="$(pwd)/../tools"
+# i want to istall boost, vsomeip to custom directory(/usr/local)
+#INSTALL_DIR="$(pwd)/../tools"
+INSTALL_DIR="/usr/local"
 
-#2.0 install Boost
-#wget https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz
-wget https://downloads.sourceforge.net/project/boost/boost/1.81.0/boost_1_81_0.tar.gz
-tar -xzvf boost_1_81_0.tar.gz
-cd boost_1_81_0
+#2.0 download Boost
+BOOST_ARCHIVE="boost_1_81_0.tar.gz"
+if [[ ! -f "$BOOST_ARCHIVE" ]]; then
+    echo "$BOOST_ARCHIVE not found. Downloading..."
+    #wget https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz
+    wget https://downloads.sourceforge.net/project/boost/boost/1.81.0/$BOOST_ARCHIVE
+    tar -xzvf $BOOST_ARCHIVE
+else
+    echo "$BOOST_ARCHIVE already exists. Skipping download."
+fi
+#2.1 install Boost
 pushd boost_1_81_0
 ./bootstrap.sh --prefix="$INSTALL_DIR"
 sudo ./b2 install
 popd
  
-#3.0 download build & install vsomeip
-rm -rf vsomeip
-git clone  https://github.com/COVESA/vsomeip.git
+#3.0 download vsomeip
+if [[ ! -d "vsomeip" ]]; then
+    echo "vsomeip folder not found. Cloning repository..."
+    git clone https://github.com/COVESA/vsomeip.git
+else
+    echo "vsomeip folder already exists. Skipping clone."
+fi
+#3.0 install vsomeip
 cd vsomeip
 mkdir build
 cmake -Bbuild -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DENABLE_SIGNAL_HANDLING=1 -DCMAKE_CXX_FLAGS="-Wno-unused-variable" .
